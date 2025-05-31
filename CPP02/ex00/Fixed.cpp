ea5109cae518d7396108659870bd296c35d5b114
Charlotte <charlotteschnx@gmail.com>
2025-05-31 14:00:54 +0200

    figuring out cpp02

--- CPP02/ex00/Fixed.cpp
+++ CPP02/ex00/Fixed.cpp
@@ -1,1 +1,34 @@
 #include "Fixed.hpp"
+#include <iostream>
+
+Fixed::Fixed() :  _fixedPointValue(0) {
+    std::cout << "Default constructor called" << std::endl;
+}
+
+// Other is the object being copied from
+Fixed::Fixed(const Fixed &other) : _fixedPointValue(other._fixedPointValue) {
+    std::cout << "Copy constructor called" << std::endl;
+}
+
+Fixed &Fixed::operator=(const Fixed &other) {
+    std::cout << "Copy assignment operator called" << std::endl;
+    if (this != &other) {
+        _fixedPointValue = other._fixedPointValue;
+    }
+    return *this;
+}
+
+Fixed::~Fixed() {
+    std::cout << "Default destructor called" << std::endl;
+}
+
+int Fixed::getRawBits( void ) const {
+    std::cout << "getRawBits member function called" << std::endl;
+    return this->_fixedPointValue;
+}
+
+void Fixed::setRawBits(int const raw) {
+    std::cout << "setRawBits member function called" << std::endl;
+    _fixedPointValue = raw;
+}
+
