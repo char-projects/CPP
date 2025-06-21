90e669e01d04bd91f1e4f34ec58094ed90697695
Charlotte Schnathmeier <cschnath@c3r1s6.42malaga.com>
2025-06-21 14:47:41 +0200

    started cpp05 ex01

---
+++ CPP05/ex01/include/Form.hpp
@@ -0,0 +1,14 @@
+#ifndef FORM_HPP
+#define FORM_HPP
+
+#include <iostream>
+
+class Form {
+    private:
+        const std::string _name;
+        bool _signed;
+        const int _signGrade;
+        const int _executeGrade;
+};
+
+#endif
