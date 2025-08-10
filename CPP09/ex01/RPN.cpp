5cdde123bae8a525c14aba484e2fdb10bb7a0633
Charlotte <charlotteschnx@gmail.com>
2025-08-10 18:08:54 +0200

    this one seemed too easy

---
+++ CPP09/ex01/RPN.cpp
@@ -0,0 +1,41 @@
+#include "RPN.hpp"
+
+RPN::RPN() {}
+
+RPN::~RPN() {}
+
+float RPN::evaluate(const std::string &expression) {
+    std::istringstream iss(expression);
+    std::string token;
+    std::stack<float> values;
+    while (iss >> token) {
+        if (std::isdigit(token[0])) {
+            values.push(std::atof(token.c_str()));
+        } else if (token.length() == 1) {
+            if (values.size() < 2) {
+                throw std::invalid_argument("Error");
+            }
+            float b = values.top(); values.pop();
+            float a = values.top(); values.pop();
+            float result = applyOperation(a, b, token[0]);
+            values.push(result);
+        } else {
+            throw std::invalid_argument("Error");
+        }
+    }
+    return (values.top());
+}
+
+float RPN::applyOperation(float a, float b, char op) {
+    switch (op) {
+        case '+': return a + b;
+        case '-': return a - b;
+        case '*': return a * b;
+        case '/':
+            if (b == 0)
+                throw std::invalid_argument("Error");
+            return a / b;
+        default:
+            throw std::invalid_argument("Error");
+    }
+}
---
+++ CPP09/ex01/RPN.hpp
@@ -0,0 +1,21 @@
+#ifndef RPN_HPP
+#define RPN_HPP
+
+#include <iostream>
+#include <sstream>
+#include <stack>
+#include <stdexcept>
+#include <cctype>
+#include <cstdlib>
+
+class RPN {
+    public:
+        RPN();
+        ~RPN();
+        float evaluate(const std::string &expression);
+        
+    private:
+        float applyOperation(float a, float b, char op);
+};
+
+#endif
