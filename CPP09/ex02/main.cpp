05d941363e2e0808df445e44e7bf39d044e3f867
Charlotte <charlotteschnx@gmail.com>
2025-08-10 19:11:22 +0200

    looks a bit better, have to test more

--- CPP09/ex02/main.cpp
+++ CPP09/ex02/main.cpp
@@ -12,7 +12,7 @@ int main(int argc, char **argv) {
         char *endptr = NULL;
         long val = std::strtol(argv[i], &endptr, 10);
         if (*endptr != '\0' || val <= 0 || val > INT_MAX) {
-            std::cerr << "Error: Invalid input '" << argv[i] << "'" << std::endl;
+            std::cerr << "Error" << std::endl;
             return 1;
         }
         vec.push_back(static_cast<int>(val));
@@ -24,13 +24,13 @@ int main(int argc, char **argv) {
     PmergeMe pmerge;
     pmerge.sortVec(vec);
     std::clock_t stop = std::clock();
-    double timeVec = static_cast<double>(stop - start) / CLOCKS_PER_SEC * 1e6;
+    double timeVec = static_cast<double>(stop - start) / CLOCKS_PER_SEC * 100;
 
     std::deque<int> deq(vec.begin(), vec.end());
     std::clock_t beginning = std::clock();
     pmerge.sortDeq(deq);
     std::clock_t end = std::clock();
-    double timeDeq = static_cast<double>(end - beginning) / CLOCKS_PER_SEC * 1e6;
+    double timeDeq = static_cast<double>(end - beginning) / CLOCKS_PER_SEC * 100;
 
     std::cout << "After:  ";
     for (size_t i = 0; i < vec.size(); ++i) {
