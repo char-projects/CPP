68eff061f8e3e62279631e9ce5d1c016eeb1f34b
Charlotte <charlotteschnx@gmail.com>
2025-09-06 15:31:38 +0200

    new changes

--- CPP07/ex01/iter.hpp
+++ CPP07/ex01/iter.hpp
@@ -8,6 +8,12 @@ void iter(T* array, size_t length, void (*func)(const T&)) {
     for (size_t i = 0; i < length; ++i) {
         func(array[i]);
     }
+
+    void iter(T* array, size_t length, void (*func)(T&)) {
+    for (size_t i = 0; i < length; ++i) {
+        func(array[i]);
+    }
+}
 }
 
 #endif
