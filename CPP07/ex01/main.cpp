7ae68369ecf251d9d3d11530bfd9ea47773b2f96
Charlotte <163297140+char-projects@users.noreply.github.com>
2025-09-06 18:03:45 +0200

    done

--- CPP07/ex01/main.cpp
+++ CPP07/ex01/main.cpp
@@ -8,24 +8,18 @@ void printString(const std::string& str) {
     std::cout << str;
 }
 
-void printConstInt(int& n) {
-    std::cout << n << " ";
-}
-
-void printConstString(std::string& str) {
-    std::cout << str;
-}
-
 int main() {
     int intArray[] = {1, 2, 3, 4, 5};
+    const int constInt[] = {5, 4, 3, 2, 1};
     std::string strArray[] = {"Hello ", "World", "!"};
-    int i[] = {42};
-    std::string s[] = {"Const ", "string"};
+    const std::string constStr[] = {"Const ", "String ", "Array"};
 
     iter(intArray, 5, printInt);
     std::cout << std::endl;
     iter(strArray, 3, printString);
     std::cout << std::endl;
-    iter(i, 1, &printConstInt);
-    iter(s, 1, &printConstString);
+    iter(constInt, 5, printInt);
+    std::cout << std::endl;
+    iter(constStr, 3, printString);
+    std::cout << std::endl;
 }
